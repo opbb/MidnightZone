9c0c1b0ab7db540b70f472a0bb228682768208ef
tomgrbz <tomgrbz@gmail.com>
2023-10-28 14:28:42 -0400

    Added mobs, mobs spawning, Mob location. Although collisions are not working with player character on enemy hit

---
+++ ShadowWizardMoneyGame/Mob.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- ShadowWizardMoneyGame/TestScene.gd
+++ ShadowWizardMoneyGame/TestScene.gd
@@ -1,5 +1,6 @@
 extends Node
 
+export(PackedScene) var mob_scene
 var score
 
 
@@ -11,6 +12,7 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$UI.show_game_over()
+	$MobTimer.stop()
 
 
 func new_game():
@@ -26,4 +28,30 @@ func _on_ScoreTimer_timeout():
 
 
 func _on_StartTimer_timeout():
+	$MobTimer.start()
 	$ScoreTimer.start()
+	
+func _on_MobTimer_timeout():
+	# Create a new instance of the Mob scene.
+	var mob = mob_scene.instance()
+
+	# Choose a random location on Path2D.
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Set the mob's position to a random location.
+	mob.position = mob_spawn_location.position
+
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
