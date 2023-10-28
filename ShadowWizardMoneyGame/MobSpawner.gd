3d06dc6526944db851945cb51b0c1088a58b18f2
tomgrbz <62816199+tomgrbz@users.noreply.github.com>
2023-10-28 18:54:04 -0400

    Change mob to area2d (#12)
    * Changed Mob to be an Area2d. Changed spawn location to match new viewport
    
    * Removed comments and physics process check for velocity removed

--- ShadowWizardMoneyGame/MobSpawner.gd
+++ ShadowWizardMoneyGame/MobSpawner.gd
@@ -1,9 +1,7 @@
 extends Node
 
 export(PackedScene) var mob_scene
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 
 var listOfMobs = []
 
@@ -26,20 +24,8 @@ func _on_MobTimer_timeout():
 	# Choose a random location on Path2D.
 	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
 	mob_spawn_location.offset = randi()
-
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
-
-	# Set the mob's position to a random location.
-	mob.position = mob_spawn_location.position
-
-	# Add some randomness to the direction.
-	direction += rand_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-
-	# Choose the velocity for the mob.
-	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
+	
+	mob.set_Properties(mob_spawn_location)
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
