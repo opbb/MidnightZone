2a8dfe02f44c839f4f1c290d799c13ef11997abe
Oliver Baer-Benson <61638242+opbb@users.noreply.github.com>
2023-10-28 16:48:39 -0400

    started enemy death function

--- ShadowWizardMoneyGame/Mob.gd
+++ ShadowWizardMoneyGame/Mob.gd
@@ -1,11 +1,5 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.playing = true
@@ -17,7 +11,11 @@ func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 	
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+	
+# Determine's whether this mob is in player shadow, returns boolean.
+func _is_Mob_in_Player_Shadow():
+	
+	return false
