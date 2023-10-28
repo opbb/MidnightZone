68d8046f59351f77a7ed60a6bdfd3ee2c47229ba
tomgrbz <tomgrbz@gmail.com>
2023-10-28 17:57:12 -0400

    Changed Mob to be an Area2d. Changed spawn location to match new viewport

--- ShadowWizardMoneyGame/Mob.gd
+++ ShadowWizardMoneyGame/Mob.gd
@@ -1,23 +1,36 @@
-extends RigidBody2D
+extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+var speed = 50
+var velocity = Vector2(0, 0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.playing = true
 	var mob_types = $AnimatedSprite.frames.get_animation_names()
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	var angle = randf() * 2 * PI
+	
+	velocity = Vector2(cos(angle), sin(angle)) * speed
 
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 	
+func set_Properties(mob_spawn_location):
+	var direction = mob_spawn_location.rotation + PI / 2
 
+	# Set the mob's position to a random location.
+	self.position = mob_spawn_location.position
+	print(position)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	self.rotation = direction 
+	velocity.rotated(direction)
+	
+func _physics_process(delta):
+	if velocity.x < 0:
+		self.position -= velocity * delta
+	else:
+		self.position += velocity * delta
+	
